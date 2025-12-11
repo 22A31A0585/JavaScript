2494810dd05c0514d668e60895121e70d8d09d12
Mohammad Anjum Sharifa <157006469+22A31A0585@users.noreply.github.com>
2025-12-11 14:29:35 +0000

    Added datatype.js file

---
+++ 01_basics/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer and it was designed for strictness 
+
+//alert(3+3) // we are using node js,not browser
+
+console.log(3+3);
+
+//code readability should be high
+
+let name="Anjum"
+let age=20
+let isLoggedIn =false
+/* primitive data types
+1.number where all int and decimal like 
+let a=10;
+let b=3.14;
+2.String used for text can used with single quotes and backticks
+let name="anjum";
+let msg='hello';
+3.Boolean True and False
+let isStudent=true
+let isAdmin =false
+4.Null intentional empty value
+let data= null;
+5.undefined variable declared but no value assigned
+let x;
+console.log(x);
+
+*/
+console.log(typeof null);// object 
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,21 @@
-console.log("Anjum")
+const  accountId = 144553
+let accountEmail="anjum@goggle.com"
+var accountPassword="12345"
+accountCity = "Jaipur" //the varibale dont need such names and int
+let accountState;
+
+// accountId = 2 // not allowed 
+console.log(accountId);
+accountEmail="hufhu@com"
+accountPassword="212121"
+accountCity="kakinada"
+
+console.log(accountId);
+/*
+Prefer not to use Var
+because of issue in block scope and functional scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
