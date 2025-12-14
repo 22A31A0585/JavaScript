841a8ba1899f17ce0541611677eab8ca96f70928
Mohammad Anjum Sharifa <157006469+22A31A0585@users.noreply.github.com>
2025-12-14 19:42:15 +0000

    Conversion of data types

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,29 @@
+let score = "anjum"
+
+//console.log(typeof score);
+//console.log(typeof {score});
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+ let isloggedout=0
+  let booleanIsLoggedout =Boolean(isloggedout)
+  console.log(booleanIsLoggedout);
+  
+  // 1 = true;0 = false
+  // " "= false
+  // "anjum"= true
+
+  let somenumber = 33
+  let stringnumber=String(somenumber)
+  console.log( typeof stringnumber);
+  
