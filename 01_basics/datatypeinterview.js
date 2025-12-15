076d10d27995191d1bb1d00e91a401db77dc7c90
Mohammad Anjum Sharifa <157006469+22A31A0585@users.noreply.github.com>
2025-12-15 18:11:16 +0000

    interview structured on datatypes

---
+++ 01_basics/datatypeinterview.js
@@ -0,0 +1,35 @@
+console.log("Hello welcome");
+//primitive (call by value)
+// 7 types : string,number,boolean,null,undefined,symbol,BigInt
+const score =100
+const scorevalue=100.3
+
+const isloggedin=false
+const outsidetemp=null//empty
+let useremail;//undefined
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id === anotherid);
+const bignumber =8584948248755984587585n
+// add n to the last digit and it becomes bigint
+
+//Reference (non primitive) directly located into the memory 
+
+//Array,Objects,Functions
+
+//these are as usual arrays which was separated with comma
+const heros =["iron man","raju","chota bhemm"]
+//these are called objects
+let myobj={
+    name: "ravi",
+    age: 23
+}
+//function decalaration with variable
+ const myfunction=function(){
+    console.log("Hello world");
+    
+ }
+
+ console.log(typeof anotherid);
+ 
