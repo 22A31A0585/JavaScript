e00633b4379d3a778b2e76ad2e4d25c99712f145
Mohammad Anjum Sharifa <157006469+22A31A0585@users.noreply.github.com>
2025-12-15 17:45:34 +0000

    data conversions

--- 01_basics/datatypeconversion.js
+++ 01_basics/datatypeconversion.js
@@ -14,10 +14,10 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
  let isloggedout=0
   let booleanIsLoggedout =Boolean(isloggedout)
-  console.log(booleanIsLoggedout);
+  //console.log(booleanIsLoggedout);
   
   // 1 = true;0 = false
   // " "= false
@@ -25,5 +25,26 @@ console.log(booleanIsLoggedIn);
 
   let somenumber = 33
   let stringnumber=String(somenumber)
-  console.log( typeof stringnumber);
+  //console.log( typeof stringnumber);
+  // operations 
+  let value =3
+  let negvalue =-value
+ // console.log(negvalue);
   
+ let str1="hello"
+ let str2=" Anjum"
+ let str3=str1 +str2
+//console.log("1"+2+2);
+//console.log(1+2+"3");
+//if string is frst all the other characters are treated as string 
+// if last the string was allocated then previous operations were implemented
+
+//console.log(true);
+//console.log(+true);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
